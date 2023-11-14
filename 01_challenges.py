ac7b55519d2572b06d19ac8da0689050951148c1
DCrawley94 <danikacrawley.dev@gmail.com>
2023-11-14 14:45:30 +0000

    add pytest skips and tidy up python files

--- 01_challenges.py
+++ 01_challenges.py
@@ -1,12 +1,16 @@
-# Change only the variables called `YOUR_REGEX_HERE`. 
-# Please do not adjust any code within the tests. 
+import pytest
+
+# Change only the variables called `YOUR_REGEX_HERE`.
+# Please do not adjust any code within the tests.
 
 import re
 
-def cat(): 
+
+def cat():
     # Your pattern should **match** a string containing the characters cat
-    YOUR_REGEX_HERE = 'x' # <- replace with an appropriate reqular expression pattern
-    return re.compile(YOUR_REGEX_HERE) 
+    YOUR_REGEX_HERE = 'x'
+    # replace 'x' with an appropriate regular expression pattern
+    return re.compile(YOUR_REGEX_HERE)
 
 
 def test_cat_matches_correctly():
@@ -26,14 +30,16 @@ def test_cat_matches_correctly():
     assert not cat().search("12123ca234")
 
 
-def at_least_5_digits(): 
-    # Your pattern should match a string or substring containing at least 5 of the 
-    # digits from 1 to 9 only
-    YOUR_REGEX_HERE = 'x' # <- replace with an appropriate reqular expression pattern
+def at_least_5_digits():
+    # Your pattern should match a string or substring containing at least 5
+    #  of the digits from 1 to 9 only
+    YOUR_REGEX_HERE = 'x'
+    # replace 'x' with an appropriate regular expression pattern
     return re.compile(YOUR_REGEX_HERE)
 
 
-def test_at_least_5_digits_matches_correctly(): 
+@pytest.mark.skip()
+def test_at_least_5_digits_matches_correctly():
     assert at_least_5_digits().match("12345")
     assert at_least_5_digits().search("56783")
     assert at_least_5_digits().search("98764").span() == (0, 5)
@@ -50,17 +56,21 @@ def test_at_least_5_digits_matches_correctly():
     assert not at_least_5_digits().search("addc6826asd")
 
 
-def starts_with_exclamation_marks(): 
-    # Your pattern should match one or more exclamation marks at the **beginning of a string**
+def starts_with_exclamation_marks():
+    # Your pattern should match one or more exclamation marks at the
+    #  **beginning of a string**
     # You should look up regex anchors for this exercise
-    YOUR_REGEX_HERE = 'x' # <- replace with an appropriate reqular expression pattern
-    return re.compile(YOUR_REGEX_HERE) 
+    YOUR_REGEX_HERE = 'x'
+    # replace 'x' with an appropriate regular expression pattern
+    return re.compile(YOUR_REGEX_HERE)
 
 
-def test_starts_with_exclamation_marks(): 
+@pytest.mark.skip()
+def test_starts_with_exclamation_marks():
     assert starts_with_exclamation_marks().match("!!!sdlasjdlajsd")
     assert starts_with_exclamation_marks().match("!!askjaa").span() == (0, 2)
-    assert starts_with_exclamation_marks().match("!!!!!adjaksljd").group() == '!!!!!'
+    assert starts_with_exclamation_marks().match(
+        "!!!!!adjaksljd").group() == '!!!!!'
     assert len(starts_with_exclamation_marks().findall("!!!32749!!!anks")) == 1
     assert starts_with_exclamation_marks().match("!abc")
 
@@ -70,22 +80,25 @@ def test_starts_with_exclamation_marks():
     assert not starts_with_exclamation_marks().match("cjljad!!!!gga!!sdd")
 
 
-def exact_6_abcs(): 
-    # Your pattern should match a string consisting of exactly 6 of a, b or c consecutively
+def exact_6_abcs():
+    # Your pattern should match a string consisting of exactly 6 of a, b or
+    #  c consecutively
     # You should look up regex anchors for this exercise
-    YOUR_REGEX_HERE = 'x' # <- replace with an appropriate reqular expression pattern
+    YOUR_REGEX_HERE = 'x'
+    # replace 'x' with an appropriate regular expression pattern
     return re.compile(YOUR_REGEX_HERE)
 
 
-def test_exact_6_abcs(): 
+@pytest.mark.skip()
+def test_exact_6_abcs():
     assert exact_6_abcs().search("abcabc")
     assert exact_6_abcs().match("cbabac")
     assert not exact_6_abcs().search("jdjdhgcacacamzmxm")
     assert not exact_6_abcs().search("bbbccakkksyyt")
-    assert not exact_6_abcs().search("xyzxyz") 
+    assert not exact_6_abcs().search("xyzxyz")
     assert not exact_6_abcs().search("pqrsqp")
     assert not exact_6_abcs().search("pprrss")
     assert not exact_6_abcs().search("vsxprh")
     assert not exact_6_abcs().search("abcabca")
-    assert not exact_6_abcs().search("abca") 
+    assert not exact_6_abcs().search("abca")
     assert not exact_6_abcs().search("jdjdhgcacacabmzmxm")
